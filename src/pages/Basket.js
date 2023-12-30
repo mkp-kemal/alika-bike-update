fa338476ce5ae66325b648f61741102bab570ccb
mkp-kemal <kemal.garut7@gmail.com>
2023-12-30 09:50:31 +0700

    add soon

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -158,11 +158,11 @@ export function Basket() {
                             {/* IS KERANJANG KOSONG */}
                             {totalRow <= 0 || totalRow == null ? (
                                 <div className="" style={{ marginTop: '100px', textAlign: 'center', alignContent: 'center', alignItems: 'center', }}>
-                                    <p className="text-secondary">Keranjang Masih Kosong</p>
+                                    <h3 className="text-secondary">Keranjang Masih Kosong</h3>
                                     <div className="text-secondary mb-5">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="m17 10l-2-6m-8 6l.75-2.252m1.001-3.002L9 4m3 4h7a2 2 0 0 1 1.977 2.304C20.535 12.82 20.221 14.742 20 16m-1.01 3.003a2.997 2.997 0 0 1-2.234.997H7.244a3 3 0 0 1-2.965-2.544l-1.255-7.152A2 2 0 0 1 5.001 8H8" /><path d="M12 12a2 2 0 1 0 2 2M3 3l18 18" /></g></svg>
                                     </div>
-                                    <button className="btn btn-info" onClick={() => { const toHome = navigate('/') }}>Beranda</button>
+                                    <button className="btn btn-info" onClick={() => { navigate('/') }}><h3 className="mt-2">Beranda</h3></button>
                                 </div>
                             ) : (
                                 <div>
